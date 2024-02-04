09350822b0da793521c2a8a160920aea19d2c483
ncedile nkonyana <ncedile.nkonyana@sovtech.com>
2024-02-04 19:04:29 +0200

    fix: adds form fields for the enquiry form. I need the the list of enquiry types

--- src/forms/enquiry-form/index.tsx
+++ src/forms/enquiry-form/index.tsx
@@ -1,9 +1,127 @@
-import { Stack } from "@mui/material"
-const EnquiryForm = () => {
-  return (
-    <Stack width="100%">
+import { 
+  Stack, 
+  TextField, 
+  Button, 
+  InputLabel, 
+  Select, 
+  MenuItem, 
+  Divider, 
+  RadioGroup
+} from "@mui/material"
+import { format } from 'date-fns'
+import { FieldValues, FormProvider, useForm } from "react-hook-form"
+import { useNavigate } from "react-router-dom"
+import FormControl from "../controls/form-control"
+import { FieldControl, RadioGroupControl } from "../controls"
+import { RadioField } from "../fields"
 
-    </Stack>
+const EnquiryFrom = () => {
+  const methods = useForm()
+  const { register, handleSubmit, formState: { errors } } = methods
+  const navigate = useNavigate()
+  
+  const submitForm = (data: FieldValues) => {
+    console.log('Submitting values....', data)
+  }
+
+  const onCancel = () => {
+    navigate("/applications", { replace: true })
+  }
+
+  return (
+    <FormProvider {...methods}>
+      <Stack width="100%" p={5} justifyContent="center" alignItems="center">
+        <form style={{ width: "100%" }} onSubmit={handleSubmit(submitForm)}>
+          <Stack width="100%" spacing={5} justifyContent="center" alignItems="center">
+            <FieldControl>
+              <TextField
+                id="first-name"
+                label="First Name"
+                error={errors?.firstname ? true : false}
+                helperText={errors?.firstname ? "First name is required" : !errors?.firstname ? "" : "Enter First Name"}
+                variant="standard"
+                {...register("firstname", { required: true })}
+              />
+            </FieldControl>
+
+            <FieldControl>
+              <TextField
+                id="last-name"
+                label="Last Name"
+                error={errors?.lastname ? true : false}
+                helperText={errors?.lastname ? "Last name is required" : !errors?.lastname ? "" : "Enter Last Name"}
+                variant="standard"
+                {...register("lastname", { required: true })}
+              />
+            </FieldControl>
+
+            <FieldControl>
+              <TextField
+                id="contact"
+                label="Contact Number"
+                error={errors?.contactNumber ? true : false}
+                helperText={errors?.contactNumber ? "Contact number is required" : !errors?.contactNumber ? "" : "Enter Contact number"}
+                variant="standard"
+                {...register("contactNumber", { required: true })}
+              />
+            </FieldControl>
+
+            <FieldControl>
+              <TextField
+                id="email"
+                type="email"
+                label="Email Address"
+                error={errors?.email ? true : false}
+                helperText={errors?.email ? "Email is required" : !errors?.email ? "" : "Enter Email Address"}
+                variant="standard"
+                {...register("email", { required: true })}
+              />
+            </FieldControl>
+
+            <FieldControl>
+              <InputLabel id="enquiryType">Enquiry Type</InputLabel>
+              <Select
+                labelId="enquiryType"
+                id="enquiryType"
+                label="Enquiry Type"
+                {...register("enquiryType")}
+              >
+                {[{
+                  label: "Type A",
+                  value: "type_a"
+                }, {
+                  label: "Type B",
+                  value: "type_b"
+                }, {
+                  label: "Type C",
+                  value: "type_c"
+                }].map((option) => (<MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>))}
+              </Select>
+            </FieldControl>
+          </Stack>
+
+          <Stack p={4} />
+
+          <Stack width="100%" flexDirection="row" gap={10} justifyContent="center" alignItems="center">
+            <Stack width="20%">
+              <Button
+                type="button"
+                variant="outlined"
+                onClick={onCancel}
+              >
+                Cancel
+              </Button>
+            </Stack>
+
+            <Stack width="20%">
+              <Button type="submit" variant="outlined">
+                Submit
+              </Button>
+            </Stack>
+          </Stack>
+        </form>
+      </Stack>
+    </FormProvider>
   )
 }
-export default EnquiryForm
+export default EnquiryFrom
